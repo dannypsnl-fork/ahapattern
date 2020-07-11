eaf1633428d443a387d9915d016301e0c101660f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-11 11:32:59 +0800

    of pattern

--- pattern.go
+++ pattern.go
@@ -18,23 +18,35 @@ type pattern struct {
 	//   func(i int) int {
 	//   	return i
 	//   }
-	ofPatterns  []interface{}
-	elsePattern interface{}
+	ofExpressions []reflect.Value
+	ofPatterns    []reflect.Value
 }
 
-func (p *pattern) Of(t interface{}, f interface{}) {
-	// NOTE: t should be `int`, `struct xxx`
-	// TODO: check f via t
+func (p *pattern) Of(e interface{}, f interface{}) *pattern {
+	rf := reflect.ValueOf(f)
+	re := reflect.ValueOf(e)
+	mustNumber(rf.Type().NumIn(), 1)
+	mustNumber(rf.Type().NumOut(), 1)
+	// first parameter
+	mustTrue(rf.Type().In(0).AssignableTo(p.typeOfTarget))
+	mustTrue(re.Type().AssignableTo(p.typeOfTarget))
+	p.ofExpressions = append(p.ofExpressions, re)
+	p.ofPatterns = append(p.ofPatterns, rf)
+	return p
 }
 
 func (p *pattern) Else(f interface{}) interface{} {
 	rf := reflect.ValueOf(f)
-	// must be a function with one parameter and one output
 	// TODO: maybe handle function with error output?
 	mustNumber(rf.Type().NumIn(), 1)
 	mustNumber(rf.Type().NumOut(), 1)
 	// first parameter
 	mustTrue(rf.Type().In(0).AssignableTo(p.typeOfTarget))
+	for i, ofPattern := range p.ofPatterns {
+		if p.target.Interface() == p.ofExpressions[i].Interface() {
+			return ofPattern.Call([]reflect.Value{p.target})[0].Interface()
+		}
+	}
 	return rf.Call([]reflect.Value{p.target})[0].Interface()
 }
 
--- pattern_test.go
+++ pattern_test.go
@@ -9,7 +9,9 @@ import (
 func TestMatch(t *testing.T) {
 	a := assert.New(t)
 
-	result := Match(1).Else(func(i int) int { return i })
+	result := Match(1).
+		Of(1, func(i int) int { return 2 }).
+		Else(func(i int) int { return i })
 
-	a.Equal(1, result)
+	a.Equal(2, result)
 }
